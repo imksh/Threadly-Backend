5bd53f8f7536d9bc1c2e9829646c3bc07c4a74e5
Karan Kumar <83858496+imksh@users.noreply.github.com>
2025-08-28 01:28:37 +0530

    Update SpringSecurity.java

--- src/main/java/com/example/journal/config/SpringSecurity.java
+++ src/main/java/com/example/journal/config/SpringSecurity.java
@@ -31,7 +31,7 @@ public class SpringSecurity {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http.authorizeHttpRequests(request -> request
-                        .requestMatchers("/public/**","/journal/**","/user/**").permitAll()
+                        .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/journal/**","/user/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll())
